9abe94ab0d28b2dddff81a3536994dcd96df12f2
PravinRaja22 <102848025+PravinRaja22@users.noreply.github.com>
2022-11-23 15:33:03 +0530

    update

--- Backend/Router/router.js
+++ Backend/Router/router.js
@@ -1,65 +1,18 @@
-const item = require('../item.js')
-//options for get all items
 
-const getItemopts={
-    schema:{
-        Response:{
-            200:{
-                type:'array',
-                item:{
-                    type:'object',
-                    properties:{
-                       id:{type:'integer'},
-                        name:{type:'string'}
-                    }
-                }
-            }
-        }
-    }
-}
-console.log(getItemopts);
-const getItemoptss= {
-    schema:{
-        Response:{
-            200:{
-                    type:'object',
-                    properties:{
-                        id:{type:'string'},
-                       // name:{type:'string'}
-                    }
-                }
-        }
-    }
-}
+const items = require('../item')
+function getdatafromreact(fastify,options,done){
+console.log(items);
+fastify.get('/',(request,reply)=>{
+reply.send(items)
+})
 
-function itemRoutes(fastify,options,done){
-//get all records
-    fastify.get('/',getItemopts,(request,reply)=>{
-          reply.send(item)
-    })
-//get id based items
-    fastify.get('/:id',getItemoptss,(request, reply) => {
-        try{
-            const { id } = request.params;
-        var paramid = item.find((paramdata) => paramdata.id == id)
-        if (paramid) {
-            reply.send(paramid)
-    
-        }
-        else if (!paramid) {
-            reply.send('No Records Found')
-        } 
-        }
-        catch(e){
-        fastify.log.error(e)
-        process.exit(1)
-        }
-    })
+fastify.post('/',(request,reply)=>{
+    reply.send("inside post method")
+})
 
     done();
 
-
-
 }
 
-module.exports = itemRoutes
+
+module.exports = getdatafromreact;
--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,5 @@
 const fastify = require('fastify')({ logger: true })
-
-fastify.register(require('./Router/router'))
+fastify.register(require('./Router/router2'))
 const start = async () => {
     try {
         await fastify.listen('3000', () => {
