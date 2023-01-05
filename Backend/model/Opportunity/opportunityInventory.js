6aa6d5fec0c40471ea8c4a3e6185f180afb03859
PravinRaja22 <102848025+PravinRaja22@users.noreply.github.com>
2023-01-05 15:13:03 +0530

    changes

--- Backend/model/Opportunity/opportunityInventory.js
+++ Backend/model/Opportunity/opportunityInventory.js
@@ -1,11 +1,11 @@
 const { MongoClient } = require('mongodb');
-async function getOpportunityInventorylookup(inventoryId) {
+async function getOpportunityInventorylookup(OpportunityId) {
     const url = "mongodb+srv://smartcrm:smart123@cluster0.rbvicx9.mongodb.net/?retryWrites=true&w=majority";
     const client = new MongoClient(url);
     try {
-        console.log("Inventory Id for Opportunity  "+inventoryId);
+        console.log("Inventory Id for Opportunity  "+OpportunityId);
         await client.connect();
-        let data = await getOpportunityDatas(client,inventoryId)
+        let data = await getOpportunityDatas(client,OpportunityId)
         return data;
     } catch (e) {
         console.error(e);
@@ -16,7 +16,7 @@ async function getOpportunityInventorylookup(inventoryId) {
 getOpportunityInventorylookup().catch(console.error);
 async function getOpportunityDatas(client,inventoryId) {
     console.log("inside functionality inventory id "+inventoryId);
-    const cursor = await client.db("CRM").collection("Opportunity Inventory").find({InventoryId :new RegExp('^' + inventoryId)})
+    const cursor = await client.db("CRM").collection("Opportunity Inventory").find({OpportunityId :new RegExp('^' + OpportunityId)})
     const results = await cursor.toArray();
     if (results.length > 0) {
         // console.log(results);
