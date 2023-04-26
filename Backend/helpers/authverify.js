f4d9e01a363602d1677aca26636f4bfcde05d810
PravinRaja22 <102848025+PravinRaja22@users.noreply.github.com>
2023-04-26 16:02:14 +0530

    update

--- Backend/helpers/authverify.js
+++ Backend/helpers/authverify.js
@@ -3,6 +3,7 @@ const { tokenValidator } = require('./jwttoken')
 let authVerify = async (req, res, next) => {
     console.log("inside token validator funciton");
    // console.log("cookie ", req.getHeader('set-cookie'));
+   console.log("inside Auth Verification Page")
     console.log(req.cookies.jwt)
     try {
         const {jwt} = await req.cookies;
