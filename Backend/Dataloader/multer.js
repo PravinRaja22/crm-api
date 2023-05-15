abe229ab0772d8f8b43d3c9b9ca69b307a2656f9
PravinRaja22 <102848025+PravinRaja22@users.noreply.github.com>
2023-05-15 15:49:15 +0530

    mule

--- Backend/Dataloader/multer.js
+++ Backend/Dataloader/multer.js
@@ -1,7 +1,7 @@
 const Multer = require('fastify-multer')
 const path = require('path')
 //const csvtojson = require('csvtojson')
-let fileMaxSize = 50 * 1024 * 1024;
+let fileMaxSize = 500 * 1024 * 1024;
 var storage = Multer.diskStorage({
     destination: (req, file, cb) => {
         const ROOT_PATH = __dirname
