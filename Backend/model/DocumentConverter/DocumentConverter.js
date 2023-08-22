796e691d4fb14b1ed1e5dc4110d53141938bfb31
Harish Raja G <harishraja411@gmail.com>
2023-08-22 12:30:45 +0530

    push

---
+++ Backend/model/DocumentConverter/DocumentConverter.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const path = require("path");
+const { exec } = require("child_process");
+const util = require("util");
+const execAsync = util.promisify(exec);
+
+const convertDocument = () => {};
